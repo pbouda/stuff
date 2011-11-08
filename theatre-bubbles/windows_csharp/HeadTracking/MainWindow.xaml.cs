0ec68db472cf85f75f9c4c06c56ac549b0eb06f0
unknown <pbouda@.(none)>
2011-11-08 15:27:44 +0000

    new projects for windows kinect

---
+++ theatre-bubbles/windows_csharp/HeadTracking/MainWindow.xaml.cs
@@ -0,0 +1,177 @@
+/////////////////////////////////////////////////////////////////////////
+//
+// This module contains code to do Kinect NUI initialization and
+// processing and also to display NUI streams on screen.
+//
+// Copyright © Microsoft Corporation.  All rights reserved.  
+// This code is licensed under the terms of the 
+// Microsoft Kinect for Windows SDK (Beta) from Microsoft Research 
+// License Agreement: http://research.microsoft.com/KinectSDK-ToU
+//
+/////////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Research.Kinect.Nui;
+
+namespace SkeletalViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Runtime nui;
+        DateTime lastTime = DateTime.MaxValue;
+        Point lastPos = new Point(0, 0);
+        
+        Dictionary<JointID,Brush> jointColors = new Dictionary<JointID,Brush>() { 
+            {JointID.HipCenter, new SolidColorBrush(Color.FromRgb(169, 176, 155))},
+            {JointID.Spine, new SolidColorBrush(Color.FromRgb(169, 176, 155))},
+            {JointID.ShoulderCenter, new SolidColorBrush(Color.FromRgb(168, 230, 29))},
+            {JointID.Head, new SolidColorBrush(Color.FromRgb(200, 0,   0))},
+            {JointID.ShoulderLeft, new SolidColorBrush(Color.FromRgb(79,  84,  33))},
+            {JointID.ElbowLeft, new SolidColorBrush(Color.FromRgb(84,  33,  42))},
+            {JointID.WristLeft, new SolidColorBrush(Color.FromRgb(255, 126, 0))},
+            {JointID.HandLeft, new SolidColorBrush(Color.FromRgb(215,  86, 0))},
+            {JointID.ShoulderRight, new SolidColorBrush(Color.FromRgb(33,  79,  84))},
+            {JointID.ElbowRight, new SolidColorBrush(Color.FromRgb(33,  33,  84))},
+            {JointID.WristRight, new SolidColorBrush(Color.FromRgb(77,  109, 243))},
+            {JointID.HandRight, new SolidColorBrush(Color.FromRgb(37,   69, 243))},
+            {JointID.HipLeft, new SolidColorBrush(Color.FromRgb(77,  109, 243))},
+            {JointID.KneeLeft, new SolidColorBrush(Color.FromRgb(69,  33,  84))},
+            {JointID.AnkleLeft, new SolidColorBrush(Color.FromRgb(229, 170, 122))},
+            {JointID.FootLeft, new SolidColorBrush(Color.FromRgb(255, 126, 0))},
+            {JointID.HipRight, new SolidColorBrush(Color.FromRgb(181, 165, 213))},
+            {JointID.KneeRight, new SolidColorBrush(Color.FromRgb(71, 222,  76))},
+            {JointID.AnkleRight, new SolidColorBrush(Color.FromRgb(245, 228, 156))},
+            {JointID.FootRight, new SolidColorBrush(Color.FromRgb(77,  109, 243))}
+        };
+
+        private void Window_Loaded(object sender, EventArgs e)
+        {
+            nui = new Runtime();
+
+            try
+            {
+                nui.Initialize(RuntimeOptions.UseDepthAndPlayerIndex | RuntimeOptions.UseSkeletalTracking | RuntimeOptions.UseColor);
+            }
+            catch (InvalidOperationException)
+            {
+                System.Windows.MessageBox.Show("Runtime initialization failed. Please make sure Kinect device is plugged in.");
+                return;
+            }
+
+
+            try
+            {
+                nui.VideoStream.Open(ImageStreamType.Video, 2, ImageResolution.Resolution640x480, ImageType.Color);
+                nui.DepthStream.Open(ImageStreamType.Depth, 2, ImageResolution.Resolution320x240, ImageType.DepthAndPlayerIndex);
+            }
+            catch (InvalidOperationException)
+            {
+                System.Windows.MessageBox.Show("Failed to open stream. Please make sure to specify a supported image type and resolution.");
+                return;
+            }
+
+            lastTime = DateTime.Now;
+
+            nui.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(nui_SkeletonFrameReady);
+        }
+
+
+        private Point getDisplayPosition(Joint joint)
+        {
+            float depthX, depthY;
+            nui.SkeletonEngine.SkeletonToDepthImage(joint.Position, out depthX, out depthY);
+            depthX = depthX * 320; //convert to 320, 240 space
+            depthY = depthY * 240; //convert to 320, 240 space
+            int colorX, colorY;
+            ImageViewArea iv = new ImageViewArea();
+            // only ImageResolution.Resolution640x480 is supported at this point
+            nui.NuiCamera.GetColorPixelCoordinatesFromDepthPixel(ImageResolution.Resolution640x480, iv, (int)depthX, (int)depthY, (short)0, out colorX, out colorY);
+
+            // map back to skeleton.Width & skeleton.Height
+            return new Point((int)(skeleton.Width * colorX / 640.0), (int)(skeleton.Height * colorY / 480));
+        }
+
+        Polyline getBodySegment(Microsoft.Research.Kinect.Nui.JointsCollection joints, Brush brush, params JointID[] ids)
+        {
+            PointCollection points = new PointCollection(ids.Length);
+            for (int i = 0; i < ids.Length; ++i )
+            {
+                points.Add(getDisplayPosition(joints[ids[i]]));
+            }
+
+            Polyline polyline = new Polyline();
+            polyline.Points = points;
+            polyline.Stroke = brush;
+            polyline.StrokeThickness = 5;
+            return polyline;
+        }
+
+        void nui_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            SkeletonFrame skeletonFrame = e.SkeletonFrame;
+            int iSkeleton = 0;
+
+            skeleton.Children.Clear();
+            foreach (SkeletonData data in skeletonFrame.Skeletons)
+            {
+                if (SkeletonTrackingState.Tracked == data.TrackingState)
+                {
+
+                    // Draw joints
+                    Boolean foundHead = false;
+                    foreach (Joint joint in data.Joints)
+                    {
+                        if (joint.ID == JointID.Head)
+                        {
+                            Point jointPos = getDisplayPosition(joint);
+                            Image bubble = new Image();
+                            ImageSource imageSource = (ImageSource)FindResource("think"); 
+                            bubble.Source = imageSource;
+                            bubble.SetValue(Canvas.TopProperty, jointPos.Y);
+                            bubble.SetValue(Canvas.LeftProperty, 1280 - jointPos.X);
+                            skeleton.Children.Add(bubble);
+                            lastPos = jointPos;
+                            foundHead = true;
+                        }
+                    }
+                    if (!foundHead)
+                    {
+                        Image bubble = new Image();
+                        ImageSource imageSource = (ImageSource)FindResource("think");
+                        bubble.Source = imageSource;
+                        bubble.SetValue(Canvas.TopProperty, 000);
+                        bubble.SetValue(Canvas.LeftProperty, 1280 - lastPos.X);
+                        skeleton.Children.Add(bubble);
+                    }
+                }
+                iSkeleton++;
+            } // for each skeleton
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            nui.Uninitialize();
+            Environment.Exit(0);
+        }
+
+    }
+}
