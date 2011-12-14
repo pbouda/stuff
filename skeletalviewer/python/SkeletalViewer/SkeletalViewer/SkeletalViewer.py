a4253eef9ce1203294029e116e54ac55669b51de
pbouda <pbouda@cidles.eu>
2011-12-14 17:10:32 +0000

    skeletal tracking in Python working.

--- skeletalviewer/python/SkeletalViewer/SkeletalViewer/SkeletalViewer.py
+++ skeletalviewer/python/SkeletalViewer/SkeletalViewer/SkeletalViewer.py
@@ -3,60 +3,72 @@ import wpf
 import clr
 clr.AddReferenceToFile("Microsoft.Research.Kinect.dll")
 
-from System import InvalidOperationException, DateTime
+from System import InvalidOperationException, DateTime, Uri, UriKind
 from System.Windows import Application, Window, Point, MessageBox
+from System.Windows.Controls import Image, Canvas
 from System.Windows.Media import Brush, SolidColorBrush, Color
+from System.Windows.Media.Imaging import BitmapImage, BitmapCacheOption
 from Microsoft.Research.Kinect.Nui import Runtime, RuntimeOptions, \
-    JointID, ImageStreamType, ImageResolution, ImageType
-
-RED_IDX = 2
-GREEN_IDX = 1
-BLUE_IDX = 0
+    JointID, ImageStreamType, ImageResolution, JointID, SkeletonTrackingState, \
+    ImageViewArea
 
 class MainWindow(Window):
     def __init__(self):
         self.gui = wpf.LoadComponent(self, 'SkeletalViewer.xaml')
         #self.create_runtime()
-        self.last_time = DateTime.MaxValue
-        self.totalFrames = 0
-        self.lastFrames = 0
-        self.depthFrame32 = list()
 
         self.Loaded += self.on_loaded
         self.Closing += self.on_closing
 
+        self.thinkBubble = BitmapImage()
+        self.thinkBubble.BeginInit()
+        self.thinkBubble.UriSource = Uri("think.png", UriKind.Relative);
+        self.thinkBubble.CacheOption = BitmapCacheOption.OnLoad;
+        self.thinkBubble.EndInit();
+
     def on_loaded(self, s, e):
         self.nui = Runtime()
-        try:
-            self.nui.Initialize(RuntimeOptions.UseDepthAndPlayerIndex | RuntimeOptions.UseSkeletalTracking | RuntimeOptions.UseColor)
-        except(InvalidOperationException):
+
+        if not self.nui:
             MessageBox.Show("Runtime initialization failed. Please make sure Kinect device is plugged in.")
             return
 
-        try:
-            self.nui.VideoStream.Open(ImageStreamType.Video, 2, ImageResolution.Resolution640x480, ImageType.Color)
-            self.nui.DepthStream.Open(ImageStreamType.Depth, 2, ImageResolution.Resolution320x240, ImageType.DepthAndPlayerIndex)
-        except(InvalidOperationException):
-            MessageBox.Show("Failed to open stream. Please make sure to specify a supported image type and resolution.");
-            return
-
-        self.last_time = DateTime.Now
-        #self.nui.DepthFrameReady += self.nui_DepthFrameReady
+        self.nui.Initialize(RuntimeOptions.UseSkeletalTracking)
         self.nui.SkeletonFrameReady += self.nui_skeleton_frame_ready
-        #self.nui.VideoFrameReady += self.nui_ColorFrameReady
+
+    def getDisplayPosition(self, joint):
+        depthX, depthY = self.nui.SkeletonEngine.SkeletonToDepthImage(joint.Position)
+        depthX = depthX * 320
+        depthY = depthY * 240
+        iv = ImageViewArea()
+
+        # only ImageResolution.Resolution640x480 is supported at this point
+        colorX, colorY = self.nui.NuiCamera.GetColorPixelCoordinatesFromDepthPixel(ImageResolution.Resolution640x480, iv, depthX, depthY, 0)
+
+        # map back to skeleton.Width & skeleton.Height
+        return Point( (self.skeleton.Width * colorX / 640.0), (self.skeleton.Height * colorY / 480) )
 
     def nui_skeleton_frame_ready(self, sender, e):
         skeletonFrame = e.SkeletonFrame
-        iSkeleton = 0
-        brushes = list()
-        brushes.append(SolidColorBrush(Color.FromRgb(255, 0, 0)))
-        brushes.append(SolidColorBrush(Color.FromRgb(0, 255, 0)))
-        brushes.append(SolidColorBrush(Color.FromRgb(64, 255, 255)))
-        brushes.append(SolidColorBrush(Color.FromRgb(255, 255, 64)))
-        brushes.append(SolidColorBrush(Color.FromRgb(255, 64, 255)))
-        brushes.append(SolidColorBrush(Color.FromRgb(128, 128, 255)))
-
         self.skeleton.Children.Clear()
+
+        for data in skeletonFrame.Skeletons:
+            if SkeletonTrackingState.Tracked == data.TrackingState:
+                foundHead = False
+                for joint in data.Joints:
+                    if joint.ID == JointID.Head:
+                        jointPos = self.getDisplayPosition(joint)
+                        bubble = Image()
+                        #imageSource = System.Windows.Resources["think"]
+                        bubble.Source = self.thinkBubble
+                        bubble.SetValue(Canvas.TopProperty, 200.0)
+                        bubble.SetValue(Canvas.LeftProperty, 1280 - jointPos.X)
+                        self.skeleton.Children.Add(bubble)
+                        lastPos = jointPos
+                        foundHead = True
+
+
+
 
     def on_closing(self, s, e):
         self.nui.Uninitialize();
--- theatre-bubbles/windows_csharp/HeadTracking/MainWindow.xaml.cs
+++ theatre-bubbles/windows_csharp/HeadTracking/MainWindow.xaml.cs
@@ -40,29 +40,6 @@ namespace SkeletalViewer
         DateTime lastTime = DateTime.MaxValue;
         Point lastPos = new Point(0, 0);
         
-        Dictionary<JointID,Brush> jointColors = new Dictionary<JointID,Brush>() { 
-            {JointID.HipCenter, new SolidColorBrush(Color.FromRgb(169, 176, 155))},
-            {JointID.Spine, new SolidColorBrush(Color.FromRgb(169, 176, 155))},
-            {JointID.ShoulderCenter, new SolidColorBrush(Color.FromRgb(168, 230, 29))},
-            {JointID.Head, new SolidColorBrush(Color.FromRgb(200, 0,   0))},
-            {JointID.ShoulderLeft, new SolidColorBrush(Color.FromRgb(79,  84,  33))},
-            {JointID.ElbowLeft, new SolidColorBrush(Color.FromRgb(84,  33,  42))},
-            {JointID.WristLeft, new SolidColorBrush(Color.FromRgb(255, 126, 0))},
-            {JointID.HandLeft, new SolidColorBrush(Color.FromRgb(215,  86, 0))},
-            {JointID.ShoulderRight, new SolidColorBrush(Color.FromRgb(33,  79,  84))},
-            {JointID.ElbowRight, new SolidColorBrush(Color.FromRgb(33,  33,  84))},
-            {JointID.WristRight, new SolidColorBrush(Color.FromRgb(77,  109, 243))},
-            {JointID.HandRight, new SolidColorBrush(Color.FromRgb(37,   69, 243))},
-            {JointID.HipLeft, new SolidColorBrush(Color.FromRgb(77,  109, 243))},
-            {JointID.KneeLeft, new SolidColorBrush(Color.FromRgb(69,  33,  84))},
-            {JointID.AnkleLeft, new SolidColorBrush(Color.FromRgb(229, 170, 122))},
-            {JointID.FootLeft, new SolidColorBrush(Color.FromRgb(255, 126, 0))},
-            {JointID.HipRight, new SolidColorBrush(Color.FromRgb(181, 165, 213))},
-            {JointID.KneeRight, new SolidColorBrush(Color.FromRgb(71, 222,  76))},
-            {JointID.AnkleRight, new SolidColorBrush(Color.FromRgb(245, 228, 156))},
-            {JointID.FootRight, new SolidColorBrush(Color.FromRgb(77,  109, 243))}
-        };
-
         private void Window_Loaded(object sender, EventArgs e)
         {
             nui = new Runtime();
@@ -74,18 +51,6 @@ namespace SkeletalViewer
             catch (InvalidOperationException)
             {
                 System.Windows.MessageBox.Show("Runtime initialization failed. Please make sure Kinect device is plugged in.");
-                return;
-            }
-
-
-            try
-            {
-                nui.VideoStream.Open(ImageStreamType.Video, 2, ImageResolution.Resolution640x480, ImageType.Color);
-                nui.DepthStream.Open(ImageStreamType.Depth, 2, ImageResolution.Resolution320x240, ImageType.DepthAndPlayerIndex);
-            }
-            catch (InvalidOperationException)
-            {
-                System.Windows.MessageBox.Show("Failed to open stream. Please make sure to specify a supported image type and resolution.");
                 return;
             }
 
@@ -110,20 +75,6 @@ namespace SkeletalViewer
             return new Point((int)(skeleton.Width * colorX / 640.0), (int)(skeleton.Height * colorY / 480));
         }
 
-        Polyline getBodySegment(Microsoft.Research.Kinect.Nui.JointsCollection joints, Brush brush, params JointID[] ids)
-        {
-            PointCollection points = new PointCollection(ids.Length);
-            for (int i = 0; i < ids.Length; ++i )
-            {
-                points.Add(getDisplayPosition(joints[ids[i]]));
-            }
-
-            Polyline polyline = new Polyline();
-            polyline.Points = points;
-            polyline.Stroke = brush;
-            polyline.StrokeThickness = 5;
-            return polyline;
-        }
 
         void nui_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
         {
@@ -146,7 +97,7 @@ namespace SkeletalViewer
                             Image bubble = new Image();
                             ImageSource imageSource = (ImageSource)FindResource("think"); 
                             bubble.Source = imageSource;
-                            bubble.SetValue(Canvas.TopProperty, jointPos.Y);
+                            bubble.SetValue(Canvas.TopProperty, 200.0);
                             bubble.SetValue(Canvas.LeftProperty, 1280 - jointPos.X);
                             skeleton.Children.Add(bubble);
                             lastPos = jointPos;
@@ -158,7 +109,7 @@ namespace SkeletalViewer
                         Image bubble = new Image();
                         ImageSource imageSource = (ImageSource)FindResource("think");
                         bubble.Source = imageSource;
-                        bubble.SetValue(Canvas.TopProperty, 000);
+                        bubble.SetValue(Canvas.TopProperty, 200.0);
                         bubble.SetValue(Canvas.LeftProperty, 1280 - lastPos.X);
                         skeleton.Children.Add(bubble);
                     }
